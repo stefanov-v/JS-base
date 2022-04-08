2e637f896b8d5447831977d1e00b1a4742495a2c
SVE <s.v.e.contact@gmail.com>
2022-04-08 21:05:53 +0500

    Практика 3: функции

---
+++ script3.js
@@ -0,0 +1,42 @@
+/* Задание на урок:
+
+1) Первую часть задания повторить по уроку
+
+2) Создать функцию showMyDB, которая будет проверять свойство privat. Если стоит в позиции
+false - выводит в консоль главный объект программы
+
+3) Создать функцию writeYourGenres в которой пользователь будет 3 раза отвечать на вопрос 
+"Ваш любимый жанр под номером ${номер по порядку}". Каждый ответ записывается в массив данных
+genres
+
+P.S. Функции вызывать не обязательно*/
+
+'use strict';
+
+let numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', ''),
+    personalMovieDB = {
+        count: numberOfFilms,
+        movies: {},
+        actors: {},
+        genres: [],
+        privat: false
+    },
+    lastMovie, MovieRating;
+
+for (let i = 0; i < 2; i++) {
+    do {
+        lastMovie = prompt('Один из последних просмотренных фильмов?', '');
+    } while (lastMovie == '' || lastMovie == null || lastMovie.length > 50);
+    MovieRating = prompt('На сколько оцените его?', '');
+    personalMovieDB.movies[lastMovie] = MovieRating;
+}
+
+if (personalMovieDB.count < 10) {
+    alert('Просмотрено довольно мало фильмов');
+} else if (personalMovieDB.count >= 10 && personalMovieDB.count <= 30) {
+    alert('Вы классический зритель');
+} else if (personalMovieDB.count > 30) {
+    alert('Вы киноман');
+} else {
+    alert('Произошла ошибка');
+}
